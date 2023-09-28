e1570053e644deed9db73fcf2086844962673a28
AshtarteKk <865189421@qq.com>
2023-09-28 09:33:17 +0800

    link utils

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
-const core = require('../lib/core')
+// const core = require('../lib/core')
 
-core()
+const utils = require('../../utils')
+
+const utils1 = utils()
+console.log("utils=>>",utils1)
+// core()
