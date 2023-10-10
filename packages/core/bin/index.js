f4d51116f78aa3bab6905f725ee1c37812dd4e42
AshtarteKk <865189421@qq.com>
2023-10-10 08:52:13 +0800

    增加importLoacl

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 
+const importLocal = require('import-local')
+const lib = require('../lib/core.js')
+
+if(importLocal(__filename)){
+    require('npmlog').info('bear','正在使用 bear cli 本地版本')
+}else{
+    lib(process.argv.slice(2))
+}
+
+
+
+
 // const core = require('../lib/core')
-
-const utils = require('../../utils')
-
-const utils1 = utils()
-console.log("utils=>>",utils1)
+// const utils = require('../../utils')
+// const utils1 = utils()
+// console.log("utils=>>",utils1)
 // core()
