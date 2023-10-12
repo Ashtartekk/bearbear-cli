c961f657cd565acb97388c82b3b64cc458195022
AshtarteKk <865189421@qq.com>
2023-10-12 11:29:35 +0800

    输出cli和node版本号 更新log模块

--- packages/core/bin/index.js
+++ packages/core/bin/index.js
@@ -2,8 +2,8 @@
 
 //require: .js/.json/.node 还支持其他所有的文件 会默认用.js的引擎进行解析
 const core = require('../lib')
-// const log = require("@bearbear-cli/log")
-const log = require('../../log')
+const log = require("@bearbear-cli/log")
+// const log = require('../../log')
 
 const coredata = core()
 
